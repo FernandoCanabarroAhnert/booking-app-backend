e293a6e086d90844f626cdead4223b62052e9151
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2025-05-23 21:47:46 -0300

    :lock: security: disables cors until the front-end is deployed

--- src/main/java/com/fernandocanabarro/booking_app_backend/security/ProjectSecurityConfig.java
+++ src/main/java/com/fernandocanabarro/booking_app_backend/security/ProjectSecurityConfig.java
@@ -63,7 +63,7 @@ public class ProjectSecurityConfig {
                 @Override
                 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedOrigins(Collections.singletonList("*"));
                     config.setAllowedMethods(Collections.singletonList("*"));
                     config.setAllowedHeaders(Collections.singletonList("*"));
                     config.setAllowCredentials(true);
