a030047092f524b4bdff891d4121184d981c3bcd
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2025-06-14 16:54:10 -0300

    :wrench: fix: setting the timezone globally to prevent bugs

--- src/main/java/com/fernandocanabarro/booking_app_backend/BookingAppBackendApplication.java
+++ src/main/java/com/fernandocanabarro/booking_app_backend/BookingAppBackendApplication.java
@@ -1,5 +1,7 @@
 package com.fernandocanabarro.booking_app_backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookingAppBackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(BookingAppBackendApplication.class, args);
 	}
 
